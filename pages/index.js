31cbc5c576be621d3d774236a62492b5ca265898
felix fedronic <fedronicfelix@gmail.com>
2022-05-13 18:49:08 +0200

    getStaticProps using file system DONE

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,27 @@
+import path from "path";
+import fs from "fs/promises";
+
 function HomePage(props) {
   const { products } = props;
 
   return (
     <ul>
-      {products.map((product) => <li key={product.id}>{product.title}</li>)}
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
 }
 
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{id: "p1", title: "product 1"}, {id: "p2", title: "product 2"}, {id: "p3", title: "product 3"}]
-    }
-  }
+      products: data.products
+    },
+  };
 }
 export default HomePage;
