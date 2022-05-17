3d8f03af53fe766b4b279f65a64ce51fb931d634
felix fedronic <fedronicfelix@gmail.com>
2022-05-17 17:33:27 +0200

    getServerSideProps for serverSide rendering DONE

---
+++ pages/user-profile.js
@@ -0,0 +1,14 @@
+function userProfile(props) {
+  return <h1>{props.userName}</h1>;
+}
+
+export default userProfile;
+
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+  return {
+    props: {
+      userName: "Félix",
+    },
+  };
+}
